38573f2e3a528be4b67f3e5144989dbfa5d22737
Cauã Kath <80467897+CauaKath@users.noreply.github.com>
2023-09-03 20:53:38 -0300

    feat: added home page some info

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'kathEmerald': '#01c38e',
+        'kathBlue': '#132d46',
+      },
+    },
   },
   plugins: [],
 }
